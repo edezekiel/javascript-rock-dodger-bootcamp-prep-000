36f88bd113e3188e0a12379baeff8f6386167581
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-23 15:24:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,12 +71,12 @@ function moveDodger(e) {
 
   if (e.which === LEFT_ARROW) {
     moveDodgerLeft()
-    e.preventDefault;
-    e.stopPropagation;
+    e.preventDefault();
+    e.stopPropagation();
   } else if (e.which === RIGHT_ARROW) {
     moveDodgerRight()
-    e.preventDefault;
-    e.stopPropagation;
+    e.preventDefault();
+    e.stopPropagation();
   }
 }
 
